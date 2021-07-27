1b8dacb679221b7bf3e1ed4aec0986bd5811e5b6
JasonDesktop <MaJason93@gmail.com>
2021-07-26 21:46:39 -0700

    This allows a list to be populated in the left column

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,11 +1,14 @@
 
-
 let noteTitle;
 let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
-let noteresponse;
+let noteresponse = [];
+let conditionalDisplay;
+let conditionalDisplay2;
+let jsonNotes;
+let textValue;
 
 if (window.location.pathname === '/notes.html') {
   noteTitle = document.querySelector('.note-title');
@@ -13,7 +16,14 @@ if (window.location.pathname === '/notes.html') {
   saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
   noteList = document.querySelectorAll('.list-container .list-group');
+  conditionalDisplay = document.querySelectorAll('list-item-title');
+  conditionalDisplay2 = document.querySelectorAll('list-group-item');
 }
+
+
+$('list-group.item').click(function() {
+   textValue = $(this).conditionalDisplay.conditionalDisplay2.text();
+});
 
 // Show an element
 const show = (elem) => {
@@ -27,7 +37,7 @@ const hide = (elem) => {
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
-console.log (activeNote);
+
 
 const getNotes = () =>
   
@@ -36,12 +46,10 @@ const getNotes = () =>
     headers: {
       'Content-Type': 'application/json',
     },
-    
-    
   });
 
-
-
+  
+  
 
 const saveNote = (note) =>
 
@@ -51,10 +59,12 @@ const saveNote = (note) =>
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
+    
   //DEVIATION FROM STANDARD CODE
   })
 .then((response) => {
-  console.log("Savenote was initiated");
+  console.log("Savenote was initiated, the following is from SaveNote");
+  console.log(note);
   if (response.ok) {
     return response.json();
   } else {
@@ -77,8 +87,8 @@ const renderActiveNote = () => {
   hide(saveNoteBtn);
 
   if (activeNote.id) {
-    noteTitle.setAttribute('note_id', activeNote.id);
-    //noteText.setAttribute('readonly', true);
+    noteTitle.setAttribute('readonly', true);
+    noteText.setAttribute('readonly', true);
     noteTitle.value = activeNote.title;
     noteText.value = activeNote.text;
   } else {
@@ -96,9 +106,11 @@ const handleNoteSave = () => {
     text: noteText.value,
   };
   saveNote(newNote).then(() => {
-    console.log("The saveNote(newNote) function was initiated " + JSON.stringify(newNote));
-    getAndRenderNotes();
-    renderActiveNote();
+    console.log( "This is the output of handleNoteSave, after saveNote is finished");
+    console.log( newNote);
+    noteresponse = newNote;
+    getAndRenderNotes(newNote);
+    renderActiveNote(newNote);
   });
 };
 
@@ -124,8 +136,7 @@ const handleNoteDelete = (e) => {
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
-  console.log(`Active Note is  ${activeNote}`);
-  renderActiveNote(activeNote);
+  renderActiveNote();
 };
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
@@ -134,6 +145,18 @@ const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
 };
+
+const handleNewNoteView2 = (note) => {
+  
+  hide(saveNoteBtn);
+    console.log(JSON.stringify(textValue));
+    noteTitle.value = textValue;
+    noteText.value = "Value-Text";
+ 
+};
+
+
+  
 
 const handleRenderSaveBtn = () => {
   if (!noteTitle.value.trim() || !noteText.value.trim()) {
@@ -146,8 +169,24 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 
 const renderNoteList = async (notes) => {
-  let jsonNotes = await notes.json();
- 
+   jsonNotes = await notes.json();
+  console.log("renderNoteList was intiated");
+  
+  let noteListName = [];
+  let noteListText = [];
+  let noteListID = [];
+
+  for (let i = 0; i < jsonNotes.length; i++) {
+    noteListName.push(jsonNotes[i].text);
+    noteListText.push(jsonNotes[i].title);
+    noteListID.push(jsonNotes[i].id);
+  }
+
+  console.log(noteListName);
+  console.log(noteListText);
+  console.log(noteListID);
+
+
   if (window.location.pathname === '/notes.html') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
@@ -158,11 +197,10 @@ const renderNoteList = async (notes) => {
   const createLi = (text, delBtn = true) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
-
     const spanEl = document.createElement('span');
     spanEl.classList.add('list-item-title');
     spanEl.innerText = text;
-    spanEl.addEventListener('click', handleNoteView);
+    spanEl.addEventListener('click', intermediaryProcessing);
 
     liEl.append(spanEl);
 
@@ -183,6 +221,7 @@ const renderNoteList = async (notes) => {
     return liEl;
   };
 
+
   if (jsonNotes.length === 0) {
     noteListItems.push(createLi('No saved Notes', false));
   }
@@ -190,11 +229,21 @@ const renderNoteList = async (notes) => {
  
 console.log(jsonNotes);
 console.log(jsonNotes.length);
-  jsonNotes.prototype.forEach(() => {
-    const li = createLi(note.title);
-    li.dataset.note = JSON.stringify(note);
-    noteListItems.push(li);
-  });
+
+
+for (let i = 0; i < jsonNotes.length; i++) {
+  const li = createLi(jsonNotes[i].title);
+  li.dataset.note = JSON.stringify(notes);
+  noteListItems.push(li); 
+}
+
+
+
+
+
+
+
+
 
 
   if (window.location.pathname === '/notes.html') {
@@ -206,6 +255,8 @@ console.log(jsonNotes.length);
 
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
+const intermediaryProcessing = () => getNotes().then(handleNewNoteView2);
+
   
 if (window.location.pathname === '/notes.html') {
   console.log("pathname is read");
@@ -214,6 +265,7 @@ if (window.location.pathname === '/notes.html') {
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
   noteText.addEventListener('keyup', handleRenderSaveBtn);
 }
+
 
 console.log("index.js has loaded");
 getAndRenderNotes();
--- Develop/server.js
+++ Develop/server.js
@@ -1,79 +1,74 @@
-const { response } = require('express');
 const express = require('express');
 const fs = require('fs');
 
 const PORT = 3001;
-const path = require ('path');
-const notes = require('./db/db.json');
+const path = require('path');
+
 const uuid = require('./helpers/uuid.js');
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/index' , (req, res) => res.sendFile(path.join(__dirname, '/public/')));
-app.get('/notes' , (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+app.get('/index', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 
-app.get('/api/notes.html', (req,res) => { 
-
-console.log(notes);
-let conversion = JSON.stringify(notes);
-
-res.json(conversion);
+app.get('/api/notes.html', (req, res) => {
+    
+    const readNotes = fs.readFileSync('./db/db.json');
+    const conversionNotes = JSON.parse(readNotes);
+    console.log("This is the output from app.get");
+    res.json(conversionNotes);
 });
 
 
-app.post('/api/notes.html', (req,res) => {
+app.post('/api/notes.html', (req, res) => {
 
     console.info(`${req.method} request received, adding notes to DB`);
+    console.info("The following line shows the req.body");
+    let NewNote;
     // There are 3 properties inside the db.json file
-    const {title, text} = req.body;
+    const { title, text } = req.body;
     // This checks to see if properties exist
     if (title && text) {
-
-    // DB constructor
-        const NewNote = {
+        NewNote = {
             title,
             text,
             note_id: uuid(),
         };
-    
+        // DB constructor
+        
+
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
             } else {
                 const parsedNotes = JSON.parse(data);
+                console.log("parsedNotes output");
                 parsedNotes.push(NewNote);
-                fs.writeFile('./db/db.json' , JSON.stringify(parsedNotes) , (writeErr) =>
-                writeErr
-                    ? console.error(writeErr)
-                    :console.info('Appended the DB')
+                
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes), (writeErr) =>
+                    writeErr
+                        ? console.error(writeErr)
+                        : console.info('Appended the DB')
                 );
-        }
+
+            
+            }
         });
-    
+    }
+  
+    res.json(NewNote);
+   
 
-    const response = {
-        status: 'success',
-        body: NewNote,
-
-    };
-
-    console.log(response);
-    res.json(response);
-} else {
-    res.json("error");
-
-    
-}
 });
 
 
-   
+
 
 
 app.listen(PORT, () =>
-console.log(`STATIC SERVER AT ${PORT}`) );
+    console.log(`STATIC SERVER AT ${PORT}`));
 
-module.exports = app; 
+module.exports = app;
